b7fd20d1147c5c18a21e2462e9417b99fde41593
guochao <463561248@qq.com>
2021-11-21 08:59:38 +0800

    init

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
-use ferris_says::say;
-use std::io::{stdout, BufWriter};
+// use ferris_says::say;
+// use std::io::{stdout, BufWriter};
+
+// fn main() {
+//     let stdout = stdout();
+//     let message = String::from("Hello fellow Rustaceans!");
+//     let width = message.chars().count();
+//     let mut writer = BufWriter::new(stdout.lock());
+//     say(message.as_bytes(), width, &mut writer).unwrap();
+// }
+
+// use std::io;
 
 fn main() {
-    let stdout = stdout();
-    let message = String::from("Hello fellow Rustaceans!");
-    let width = message.chars().count();
-    let mut writer = BufWriter::new(stdout.lock());
-    say(message.as_bytes(), width, &mut writer).unwrap();
+    // println!("Guess the number!");
+
+    // println!("Please input your guess.");
+
+    // let mut guess = String::new();
+
+    // io::stdin().read_line(&mut guess)
+    //     .expect("Failed to read line");
+
+    // println!("You guessed: {}", guess);
+
+    // 加法
+    let sum = 5.0 + 10.0;
+    println!("加法---: {}", sum);
 }
