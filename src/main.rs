e2bb8094d03c601c234ebd633f265f2e9c3d1f66
guochao <463561248@qq.com>
2021-11-22 22:11:35 +0800

    ✨ feat: 了解所有权

--- src/main.rs
+++ src/main.rs
@@ -24,6 +24,27 @@ fn main() {
     // println!("You guessed: {}", guess);
 
     // 加法
-    let sum = 5.0 + 10.0;
-    println!("加法---: {}", sum);
+    // let sum = 5.0 + 10.0;
+    // println!("加法---: {}", sum);
+
+    // let mut s = String::from("hello");
+
+    // s.push_str(", world!"); // push_str() 在字符串后追加字面值
+
+    // println!("{}", s); // 将打印 `hello, world!`
+
+    // 移动
+    // let s1 = String::from("hello");
+    // let s2 = s1;
+    // println!("{}, world!", s1);
+
+    // 克隆
+    // let s1 = String::from("hello");
+    // let s2 = s1.clone();
+    // println!("s1 = {}, s2 = {}", s1, s2);
+
+    // 只在栈上的数据：拷贝
+    // let x = 5;
+    // let y = x;
+    // println!("x = {}, y = {}", x, y);
 }
